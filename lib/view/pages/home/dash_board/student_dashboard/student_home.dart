3aa804961f63c78ed219fe849ed5382e051d549e
RESHMASURESHS <reshmasuresh8089@gmail.com>
2023-11-17 13:55:24 +0530

    secondcommit

---
+++ lib/view/pages/home/dash_board/student_dashboard/student_home.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class StudentHomePage extends StatelessWidget {
+  const StudentHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //body: SafeArea(child: ),
+    );
+  }
+}
